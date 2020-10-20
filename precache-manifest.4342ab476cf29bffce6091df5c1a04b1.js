c57b8f1602179585011eab85c4ece89215bdde46
unknown <mihai.sereanu@gmail.com>
2020-10-20 14:32:03 +0300

    Updates

---
+++ precache-manifest.4342ab476cf29bffce6091df5c1a04b1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57a73fa50d40043df456817cdbfbe0a4",
+    "url": "/react_pokedex/index.html"
+  },
+  {
+    "revision": "052620ba285dd4b354ba",
+    "url": "/react_pokedex/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "a1e2dfa33e0ed3630a98",
+    "url": "/react_pokedex/static/css/main.a9546af3.chunk.css"
+  },
+  {
+    "revision": "052620ba285dd4b354ba",
+    "url": "/react_pokedex/static/js/2.19009d7a.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/react_pokedex/static/js/2.19009d7a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a1e2dfa33e0ed3630a98",
+    "url": "/react_pokedex/static/js/main.1816a7e2.chunk.js"
+  },
+  {
+    "revision": "3eefcf062d4a4fb5f3ff",
+    "url": "/react_pokedex/static/js/runtime-main.6c9f8fe5.js"
+  }
+]);
